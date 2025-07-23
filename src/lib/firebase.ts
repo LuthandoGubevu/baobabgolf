9a14e9aa4e91af2e4b903b71e0ec364135039e9c
Keegan Baobabb <keeganbaobabb@gmail.com>
2025-07-23 15:17:09 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,13 +6,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBuehBkaPES_UHO9SJ-ag43AyejWNVudoA",
-  authDomain: "addhope-golfday.firebaseapp.com",
-  projectId: "addhope-golfday",
-  storageBucket: "addhope-golfday.appspot.com",
-  messagingSenderId: "632881170544",
-  appId: "1:632881170544:web:943fc5502e251eb0640cb5",
-  measurementId: "G-SQ2CMM306T"
+  apiKey: "AIzaSyAPiz21odg4cknDAJmeik28rPfDPsTv6Ew",
+  authDomain: "baobabgolf.firebaseapp.com",
+  projectId: "baobabgolf",
+  storageBucket: "baobabgolf.appspot.com",
+  messagingSenderId: "78161841879",
+  appId: "1:78161841879:web:08cdff541d18324fec2a92",
+  measurementId: "G-J7WQB1TXEL"
 };
 
 // Initialize Firebase
